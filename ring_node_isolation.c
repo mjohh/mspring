a10141c23e7da2093ba308d07c9b03add7e93557
mjohh <mjohh@163.com>
2013-12-18 18:30:07 +0800

    fix compile error

--- ring_node_isolation.c
+++ ring_node_isolation.c
@@ -8,7 +8,7 @@
 #include <assert.h>
 
 // only sf and fs need squelch
-int is_interface_unavailabe(struct aps_controller* aps, enume side side) {
+int is_interface_unavailabe(struct aps_controller* aps, enum side side) {
     assert(aps && (WEST == side || EAST == side));
     if (is_cut(aps, side) || (EXT_CMD_CMD == EX_FS && EXT_CMD_SIDE == side))// tail
         return 1;
@@ -17,11 +17,11 @@ int is_interface_unavailabe(struct aps_controller* aps, enume side side) {
     return 0;
 }
 
-int is_recv_brq_from_nonneighbour_on_long_side(struct aps_controller* aps, enume longside) {
+int is_recv_brq_from_nonneighbour_on_long_side(struct aps_controller* aps, enum side longside) {
     enum side shortside = (longside == WEST ? EAST : WEST);
-    assert(aps && (WEST == side || EAST == longside));
+    assert(aps && (WEST == longside || EAST == longside));
     if ((DRV_KBYTES_BRCODE(longside) == SF || DRV_KBYTES_BRCODE(longside) == FS) &&
-        DRV_KBYTES_SRC(longside) != NEIB_NODE_ID(shortside)
+        DRV_KBYTES_SRC(longside) != NEIB_NODE_ID(shortside))
         return 1;
     return 0;
 }
@@ -42,14 +42,14 @@ int is_recv_brq_from_nonneighbours_both_sides(struct aps_controller* aps) {
 // from the other side it recieves KBytes not from his neighbor.
 // node is isolated if it recieves BRQ (FS,SF) from both side
 // from nodes that are not neighbors.
-eint is_isolated(struct aps_controller* aps) {
+int is_isolated(struct aps_controller* aps) {
   int west_unavailable = is_interface_unavailabe(aps, WEST);
   int east_unavailable = is_interface_unavailabe(aps, EAST);
   if (east_unavailable && east_unavailable)
      return 1;
-  if (east_unavailable && is_recv_brq_from_nonneighbour_on_long_side(WEST))
+  if (east_unavailable && is_recv_brq_from_nonneighbour_on_long_side(aps, WEST))
      return 1;
-  if (west_unavailable && is_recv_brq_from_nonneighbour_on_long_side(EAST))
+  if (west_unavailable && is_recv_brq_from_nonneighbour_on_long_side(aps, EAST))
      return 1;
   if (is_recv_brq_from_nonneighbours_both_sides(aps))
      return 1;
@@ -59,7 +59,7 @@ eint is_isolated(struct aps_controller* aps) {
 int get_neib_node(struct aps_controller* aps, int nodeid, enum side side) {
     int i;
     for (i = 0; i < aps->nodes_num; i++) {
-        if (aps->ring_map[i] = nodeid)
+        if (aps->ring_map[i] == nodeid)
             break;
     }
     assert(i < aps->nodes_num);
@@ -86,10 +86,10 @@ void get_isolated_nodes(struct aps_controller* aps, int isolated_nodes[], int* n
     if (east_unavailable && east_unavailable) {
         first_node = NEIB_NODE_ID(EAST);
         last_node = NEIB_NODE_ID(WEST);
-    } else if (east_unavailable && is_recv_brq_from_nonneighbour_on_long_side(WEST)) {
+    } else if (east_unavailable && is_recv_brq_from_nonneighbour_on_long_side(aps, WEST)) {
         first_node = NEIB_NODE_ID(EAST);
         last_node = get_neib_node(aps, DRV_KBYTES_SRC(WEST), WEST);
-    } else if (west_unavailable && is_recv_brq_from_nonneighbour_on_long_side(EAST)) {
+    } else if (west_unavailable && is_recv_brq_from_nonneighbour_on_long_side(aps, EAST)) {
         first_node = get_neib_node(aps, DRV_KBYTES_SRC(EAST), EAST);
         last_node = NEIB_NODE_ID(WEST);
     } else if (is_recv_brq_from_nonneighbours_both_sides(aps)) {
